d5c8c9ad9a4d985575dbe0fc2f05993deff770a8
Shengyu Li <shspku365@gmail.com>
2021-06-23 20:34:17 +0800

    完成最基础的md编辑器，但是样式还有问题

--- src/Markdown.js
+++ src/Markdown.js
@@ -2,15 +2,25 @@ import React, { useState } from 'react';
 import markdownIt from 'markdown-it';
 import "./styles.css";
 
-const md = new markdownIt()
+const md = new markdownIt();
 
 export default function Markdown() {
 
   const [htmlString, setHtmlString] = useState('');
+
+  const parse = (text) => setHtmlString(md.render(text));
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
+    <div className="mdContainer">
+      <textarea
+        className="edit"
+        onChange={(e)=> parse(e.target.value)} // 编辑区内容每次改变更新htmlString
+      >
+      </textarea>
+      <div
+        className="show"
+        dangerouslySetInnerHTML={{__html: htmlString}} // html字符串解析成真正的html标签
+      >
+      </div>
     </div>
   );
 }
